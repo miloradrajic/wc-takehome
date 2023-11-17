5891b54ce9c307cd8cb42d2798e087fa931b573d
Milorad Rajic <rajic.milorad@gmail.com>
2023-11-17 08:28:21 +0100

    Editing header. Added cart and login links

--- header.php
+++ header.php
@@ -15,8 +15,6 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
 </head>
 
@@ -26,34 +24,68 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'starter' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<div class="headerTop container">
+			<div class="site-branding">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$starter_description = get_bloginfo( 'description', 'display' );
-			if ( $starter_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $starter_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+					the_custom_logo();
+					if ( is_front_page() && is_home() ) :
+						?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php
+					else :
+						?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php
+					endif;
+					$starter_description = get_bloginfo( 'description', 'display' );
+					if ( $starter_description || is_customize_preview() ) :
+						?>
+						<p class="site-description"><?php echo $starter_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+					<?php endif; ?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'starter' ); ?></button>
-			<?php
+			<div class="loginLink">
+				<?php custom_login_link(); ?>
+			</div><!-- .loginLink -->
+			
+			<?php // Show cart
+				// Check if WooCommerce is active
+				if (class_exists('WooCommerce')){
+					global $woocommerce;
+					?>
+					<div class="cart-wrapper">
+						<a class="cart-link" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'starter'); ?>">
+							<?php
+							$item_count = $woocommerce->cart->get_cart_contents_count();
+							$cart_icon = '<span class="dashicons dashicons-cart"></span>';
+							echo $cart_icon;
+							if ($item_count > 0) {
+									?>
+									<span class="cart-count"><?php echo esc_html($item_count); ?></span>
+									<?php
+							}
+							?>
+						</a>
+					</div><!-- .cart-wrapper -->
+				<?php }; ?>
+		</div><!-- .headerTop -->
+
+	<nav id="site-navigation" class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container">
+			<div class="navWrapper">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php _e( 'Primary Menu', 'starter' ); ?>">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<?php
 			wp_nav_menu(
 				array(
 					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+					'menu_id'        => 'navbarSupportedContent',
+					'menu_class'		 => 'collapse navbar-collapse'
+					)
+				);
+				?>
+			</div>
+		</div>
+	</nav><!-- #site-navigation -->
+</header><!-- #masthead -->
