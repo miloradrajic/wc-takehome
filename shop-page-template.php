7a15aa3192c2ef82d1c42fcd8827119876e3c578
Milorad Rajic <rajic.milorad@gmail.com>
2023-11-17 12:17:38 +0100

    Fix for shop page not displaying main content

--- shop-page-template.php
+++ shop-page-template.php
@@ -11,6 +11,17 @@ Template Name: Custom Shop page Template
 
 		<div class="container">
 			<?php
+				// Get the ID of the WooCommerce shop page
+				$shop_page_id = wc_get_page_id('shop'); 
+				$shopPost = get_post($shop_page_id);
+
+				?>
+					<header class="entry-header">
+						<h1 class="entry-title"><?php echo get_the_title($shop_page_id); ?></h1>	
+					</header>
+				<?php	
+				
+				echo $shopPost->post_content;
 				// Include WooCommerce product archive content
 				echo do_shortcode('[products]');
 			?>
