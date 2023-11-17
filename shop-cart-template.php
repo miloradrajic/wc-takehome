2a5418d26856592f490038b4a74bffb63a1c6e48
Milorad Rajic <rajic.milorad@gmail.com>
2023-11-17 12:25:27 +0100

    FIx for checkout and empty cart on second lang

--- shop-cart-template.php
+++ shop-cart-template.php
@@ -10,20 +10,28 @@ get_header();
 			<?php
 			while ( have_posts() ) :
 					the_post();
-
           $current_language = pll_current_language();
           // Get the main language code
           $main_language = pll_default_language(); // This function returns the main language code
     
           // Check if the current language is not the main language
-          if ($current_language !== $main_language) {
-            ?>
+          if ($current_language !== $main_language) { ?>
             <header class="entry-header">
               <h1 class="entry-title"><?php the_title(); ?></h1>	
             </header>
             <div class="woocommerce">
               <div class="woocommerce-notices-wrapper"></div>
-              <?php wc_get_template_part('cart/cart'); ?>
+              <?php 
+              $cart_count = WC()->cart->get_cart_contents_count();
+
+              // Check if the cart is empty
+              if ($cart_count === 0) {
+                // If the cart is empty, use the empty_cart template
+                wc_get_template_part('cart/cart-empty');
+              } else {
+                wc_get_template_part('cart/cart');
+              } 
+              ?>
             </div><!-- .woocommerce -->
             <?php
           }else{
